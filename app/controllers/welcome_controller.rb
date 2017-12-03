6fa71f9b29f4b656b3fcc8c33e9db43316091188
Bart De Nef <bartdn183@gmail.com>
2017-12-03 17:48:55 -0500

    update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,8 +6,10 @@ class WelcomeController < ApplicationController
      if !params[:name].nil? && !params[:email].nil? && !params[:email].nil? && !params[:message].nil? && !params[:tel].nil? 
 	  	c = ContactForm.new(:name => params[:name], :email => params[:email], :message => params[:message], :tel => params[:tel])
 	  	c.deliver
+	  	flash[:notice] = "Post sent"
   	  	redirect_to root_url
   	 else
+	  	flash[:notice] = "Post not sent"
   	 	redirect_to root_url
   	 end
   end
