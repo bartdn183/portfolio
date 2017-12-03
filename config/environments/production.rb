c4f08dd3d6a5b76b39cd40ffeedf31706487735b
Bart De Nef <bartdn183@gmail.com>
2017-12-03 17:14:20 -0500

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,10 +69,10 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: false
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
